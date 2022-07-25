bdea2bdd3e45d9b0dc043dd3e7cebfba2cd6bd8b
charlie <gn4rtistic@gmail.com>
2022-07-25 01:33:44 -0700

    defined tag column in tag.js

--- models/Product.js
+++ models/Product.js
@@ -33,8 +33,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
         key: 'id',
+        model: 'category',
       },
     },
   },
