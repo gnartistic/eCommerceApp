559a554a6b008df834daf8f0f7c43af76f8b12f8
charlie <gn4rtistic@gmail.com>
2022-07-25 02:07:14 -0700

    connect to jawsdb

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,14 +2,37 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+router.post('/', (req, res) => {
+  // create a new category
+})
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const data = await Category.findAll();
+    if (!data) {
+      res.status(404).json({ message: 'Category with this id does not exist!' });
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const data = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    if (!data) {
+      res.status(404).json({ message: 'Category with this id does not exist!' });
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // be sure to include its associated Products
 });
 
